9fd1b26fb750e9037e2d9bfe30e6b4f4df3237a9
Vitaxses <151165512+Vitaxses@users.noreply.github.com>
2026-08-16 21:35:25 +0200

    Make WeaverSceneDataHandler cached (I think)

--- Data/WeaverSceneDataHandler.cs
+++ Data/WeaverSceneDataHandler.cs
@@ -1,12 +1,15 @@
+using TeamCherry.SharedUtils;
 using WorldWeaver.Managers;
 
 namespace WorldWeaver.Data;
 
-public abstract class WeaverSceneDataHandler
+public abstract class WeaverSceneDataHandler : IIncludeVariableExtensions
 {
     public abstract string ModIdentifier { get; }
 
     public abstract SceneData? GetSceneData();
+
+    public string LastSetFieldName = string.Empty;
 
     public WeaverSceneDataHandler()
     {
@@ -19,5 +22,10 @@ public abstract class WeaverSceneDataHandler
     public virtual void Init()
     {
         WeaverDataManager.TryAddDataHandler(this);
+    }
+
+    public virtual void OnUpdatedVariable(string variableName)
+    {
+        LastSetFieldName = variableName;
     }
 }
